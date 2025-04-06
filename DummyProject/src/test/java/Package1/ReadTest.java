4ff88ca74d748c2c31778744585f8d59c1a2c811
prash <prash@192.168.1.137>
2025-04-06 18:25:30 +0530

    Dummy project

---
+++ DummyProject/src/test/java/Package1/ReadTest.java
@@ -0,0 +1,13 @@
+package Package1;
+
+import org.testng.annotations.Test;
+
+public class ReadTest {
+	
+	@Test
+	public void WriteTest()
+	{
+		System.out.println("Write test executed successfully");
+	}
+
+}
---
+++ DummyProject/src/test/java/Package1/WriteTest.java
@@ -0,0 +1,13 @@
+package Package1;
+
+import org.testng.annotations.Test;
+	
+	public class WriteTest {
+		
+		@Test
+		public void ReadTest()
+		{
+			System.out.println("Read test executed successfully");
+		}
+
+}
